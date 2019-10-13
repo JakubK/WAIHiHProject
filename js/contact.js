8b06602809086092e2be0e55b90fa20ab7e9e111
JakubK <jakubkuniszewski1@gmail.com>
2019-10-13 18:20:37 +0200

    Disable Form if it was sent already

--- js/contact.js
+++ js/contact.js
@@ -1,29 +1,38 @@
-$('#validationError').text(sessionStorage.getItem('validationError'));
+if(localStorage.getItem("formSent") == "true")
+{
+  $('#validationError').text("Twoja wiadomość została wysłana.");
+  $('form').remove();
+}
+
+if(sessionStorage.getItem('validationError'))
+  $('#validationError').text(sessionStorage.getItem('validationError'));
 
 $( "input[type=submit]").button();
-
 $( function() {
     $( "#dialog" ).dialog({
       autoOpen: false
     });
   } );
 
-$('form').on('submit',function(){
-
-    
+$('form').on('submit',function(e){
+    e.preventDefault();
     let name = document.forms[0]["name"].value;
     let email = document.forms[0]["email"].value;
     let message = document.forms[0]["message"].value;
 
     if(name == "" || email == "" || message == "")
     {
-      sessionStorage.setItem("validationError", "You need to fill every field");
+      sessionStorage.setItem("validationError", "Wszystkie pola muszą być uzupełnione");
       $('#validationError').text(sessionStorage.getItem('validationError'));
       return;
     }
 
     sessionStorage.clear();
-    $('#validationError').text(sessionStorage.getItem('validationError'));
-    $('#dialog').dialog("open");
-})
+    localStorage.setItem("formSent",true);
 
+    this.submit();
+
+    $('#dialog').dialog("open");
+    $('#validationError').text("Twoja wiadomość została wysłana.");
+    $('form').remove();
+});
