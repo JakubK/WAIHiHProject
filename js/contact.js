70cc0cdb82e25e8f52897a4e605f160d087fb7b9
JakubK <jakubkuniszewski1@gmail.com>
2019-10-13 16:41:52 +0200

    Add validation code based on sessionStorage

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,5 @@
+$('#validationError').text(sessionStorage.getItem('validationError'));
+
 $( "input[type=submit]").button();
 
 $( function() {
@@ -7,5 +9,21 @@ $( function() {
   } );
 
 $('form').on('submit',function(){
+
+    
+    let name = document.forms[0]["name"].value;
+    let email = document.forms[0]["email"].value;
+    let message = document.forms[0]["message"].value;
+
+    if(name == "" || email == "" || message == "")
+    {
+      sessionStorage.setItem("validationError", "You need to fill every field");
+      $('#validationError').text(sessionStorage.getItem('validationError'));
+      return;
+    }
+
+    sessionStorage.clear();
+    $('#validationError').text(sessionStorage.getItem('validationError'));
     $('#dialog').dialog("open");
 })
+
