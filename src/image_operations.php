8da9e542df9cce2d532519e90c36aea98f31dfcf
JakubK <jakubkuniszewski1@gmail.com>
2019-12-30 18:31:33 +0100

    Shorten validate.php and add alpha support for image_operations

--- src/image_operations.php
+++ src/image_operations.php
@@ -12,7 +12,11 @@ function generate_thumbnail($source,$mime_type, $upload_dir)
     $desired_height = 125;
     $desired_width = 200;
     $virtual_image = imagecreatetruecolor($desired_width, $desired_height);
+    
+    imagealphablending($virtual_image,false);
     imagecopyresampled($virtual_image, $source_image, 0, 0, 0, 0, $desired_width, $desired_height, $width, $height);
+    imagesavealpha($virtual_image,true);
+
     if($mime_type === 'image/png')
         imagepng($virtual_image, $upload_dir);
     else
@@ -32,6 +36,8 @@ function generate_watermark($source,$mime_type, $upload_dir)
   $white = imagecolorallocatealpha($source_image,255,255,255,75);
   $font = "../../arial.ttf";
   imagettftext($source_image, 20, -45, 10, 20, $white, $font, $_POST['watermark']);
+  imagesavealpha($source_image,true);
+
   if($mime_type === 'image/png')
     imagepng($source_image, $upload_dir);
   else
--- src/validate.php
+++ src/validate.php
@@ -2,20 +2,14 @@
 
 function validate_gallery_input()
 {
-  if(empty($_FILES['file']['name']))
-  {
-      return "Brak pliku";
-  }
-  else if(empty(($_POST['watermark'])))
-  {
-      return "Tekst dla znaku wodnego jest wymagany";
-  }
-  else if($_FILES['file']['size'] > 1000000)
-  {
-      return "Plik za duży. Maksymalny rozmiar to 1MB";
-  }
+    if(empty($_FILES['file']['name']))
+        return "Brak pliku";
+    else if(empty(($_POST['watermark'])))
+        return "Tekst dla znaku wodnego jest wymagany";
+    else if($_FILES['file']['size'] > 1000000)
+        return "Plik za duży. Maksymalny rozmiar to 1MB";
 
-  return NULL;
+    return NULL;
 }
 
 ?>
