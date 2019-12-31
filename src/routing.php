ca8b1664e75d9971ca339d6617b3639ef2526ce7
JakubK <jakubkuniszewski1@gmail.com>
2019-12-31 20:42:46 +0100

    Remove code from MVC template

--- src/routing.php
+++ src/routing.php
@@ -2,13 +2,6 @@
 
 $routing = [
     '/' => 'home',
-    '/products' => 'products',
-    '/view' => 'product',
-    '/edit' => 'edit',
-    '/delete' => 'delete',
-    '/cart' => 'cart',
-    '/cart/clear' => 'clear_cart',
-    '/cart/add' => 'add_to_cart',
     '/register' => 'register_user',
     '/marked' => 'marked_gallery',
     '/logout' => 'logout',
